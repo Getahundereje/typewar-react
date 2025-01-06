c71f91ae763800d1e30ee66163991f2f826270d9
getahundereje <getahundereje123@gmail.com>
2025-01-05 17:44:28 -0800

    add input validation for sign in and sign up

--- src/components/form-input/form-input.componet.jsx
+++ src/components/form-input/form-input.componet.jsx
@@ -1,11 +1,14 @@
 import "./form-input.styles.css";
 
 // eslint-disable-next-line react/prop-types
-function FormInput({ id, label, ...otherProps }) {
-  console.log(label);
+function FormInput({ id, label, isValid, ...otherProps }) {
   return (
     <div className="input-group">
-      <input className="form-input" id={id} {...otherProps} />
+      <input
+        className={`form-input ${isValid ? "valid-input" : ""}`}
+        id={id}
+        {...otherProps}
+      />
       <label className="form-input-label" htmlFor={id}>
         {label}
       </label>
--- src/pages/sign-in-up/sign-in/sign-in.page.jsx
+++ src/pages/sign-in-up/sign-in/sign-in.page.jsx
@@ -4,8 +4,44 @@ import FormInput from "../../../components/form-input/form-input.componet";
 import FormCustomButton from "../../../components/form-custom-button/form-custom-button.component";
 
 import "../sign-in-up.styles.css";
+import { useEffect, useState } from "react";
 
 function SignIn() {
+  const [email, setEamil] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
+
+  function handleChanges(e) {
+    e.preventDefault();
+
+    const { name, value } = e.target;
+    if (name === "email") {
+      setEamil(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  }
+
+  useEffect(() => {
+    function validateInput() {
+      const emailRegex =
+        // eslint-disable-next-line no-control-regex
+        /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+
+      if (emailRegex.test(email)) {
+        setEmailIsValid(true);
+      } else setEmailIsValid(false);
+
+      if (password.length >= 8) {
+        setPasswordIsValid(true);
+      } else setPasswordIsValid(false);
+    }
+
+    validateInput();
+  }, [email, password]);
+
   return (
     <>
       <section className="container">
@@ -19,6 +55,9 @@ function SignIn() {
             id="email"
             placeholder=""
             label="Email"
+            value={email}
+            isValid={emailIsValid}
+            onChange={handleChanges}
           />
           <FormInput
             type="password"
@@ -26,6 +65,9 @@ function SignIn() {
             id="password"
             placeholder=""
             label="Password"
+            value={password}
+            isValid={passwordIsValid}
+            onChange={handleChanges}
           />
           <FormCustomButton type="submit" className="form-button">
             Sign In
